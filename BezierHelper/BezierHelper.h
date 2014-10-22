7066f83e96b9d0f18ef9c2f412c29bfbc2181c45
Zef Houssney <zef@madebykiwi.com>
2014-10-21 19:58:31 -0600

    Initial Commit

---
+++ BezierHelper/BezierHelper.h
@@ -0,0 +1,19 @@
+//
+//  BezierHelper.h
+//  BezierHelper
+//
+//  Created by Josef Houssney on 10/21/14.
+//  Copyright (c) 2014 Made by Kiwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BezierHelper.
+FOUNDATION_EXPORT double BezierHelperVersionNumber;
+
+//! Project version string for BezierHelper.
+FOUNDATION_EXPORT const unsigned char BezierHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BezierHelper/PublicHeader.h>
+
+
